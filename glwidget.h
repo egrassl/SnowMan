fcac4362d9e40798f8f06f201383859472aa86be
Eric Grassl <coala@Erics-MacBook-Pro.local>
2017-04-24 16:29:17 -0300

    Protótipo

---
+++ glwidget.h
@@ -0,0 +1,17 @@
+#ifndef GLWIDGET_H
+#define GLWIDGET_H
+
+#include <QWidget>
+
+class GLWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit GLWidget(QWidget *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // GLWIDGET_H
